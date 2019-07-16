7a0ed203ac497d1f9b3fcf3e35c4880a34b532fe
Hanna Hlibova <ahatminskaya@gmail.com>
2019-07-16 15:32:35 +0300

    adding code

--- app.js
+++ app.js
@@ -35,6 +35,39 @@ const prevSibling = document.querySelector('ul').previousSibling;
 const nextSibling = document.querySelector('ul').nextSibling;
 
 
+// 5.
+// По нажатию на кнопку "btn-msg" должен появиться алерт с тем текстом который находится в атрибуте data-text у кнопки.
+
+let msgBtn = document.querySelector('#btn-msg');
+
+msgBtn.addEventListener('click', showBtnText);
+
+function showBtnText(e) {
+    e.preventDefault();
+    alert(this.innerHTML);
+}
+
+
+// 6. 
+// При нажатии на любой узел документа показать в элементе с id=tag имя тега нажатого элемента.
+
+const allElements = Array.from(document.getElementsByTagName('*'));
+const showTagElement = document.getElementById('tag');
+let tagName = document.createElement('span');
+
+showTagElement.appendChild(tagName);
+
+for (let i = 0; i < allElements.length; i++) {
+    allElements[i].addEventListener('click', showTagName);
+}
+
+function showTagName(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    tagName.innerHTML = ' ' + e.target.tagName;
+}
+
+
 // 4. 
 // Дан массив пользователей, его можно скопировать отсюда из первой задачи, создать таблицу вида:
 // Условия:
@@ -44,7 +77,6 @@ const nextSibling = document.querySelector('ul').nextSibling;
 // В коде у вас должна быть переменная которая будет содержать в виде объекта список полей и заголовков th которые будут выводиться в таблице. 
 // Что то типа { name: ‘Name’, email: ‘Email’... } соответственно ключ объекта это ваше поле которое вы хотите вывести из объекта пользователя а значение это заголовок th.
 
-// const map = ["_id", "name", "isActive", "balance"];
 const users = [
   {
     "_id": "5d220b10e8265cc978e2586b",
@@ -87,95 +119,148 @@ const users = [
   }
 ];
 
-const tableHolder = document.querySelector('.table-holder');
-const [{_id, name, email, balance}] = users;
-const tableHeadInfo = {
+(function(arrOfUsers) {
+  // UI elements
+  const tableHolder = document.querySelector('.table-holder');
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const tableBody = document.createElement('tbody');
+  const tableFoot = document.createElement('tfoot');
+
+  //UI set styles
+  table.classList.add('table');
+  
+  const tableHeadInfo = {
     id: '#',
     name: 'Name',
     email: 'Email',
     balance: 'Balance'
-}
-const tableHeadInfoArr = Object.values(tableHeadInfo);
-const usersInfoArr = users.map(function(item) {
-  return {
-    id: item._id,
-    name: item.name,
-    email: item.email,
-    balance: item.balance
   }
-});
-
-let usersInfoTable = '<table class="table"><thead><tr>';
-  tableHeadInfoArr.forEach(item => {
-    usersInfoTable += `<th>${item}</th>`;
+  const tableHeadInfoArr = Object.values(tableHeadInfo);
+  const usersInfoArr = users.map(function(item) {
+    return {
+      id: item._id,
+      name: item.name,
+      email: item.email,
+      balance: item.balance
+    }
   });
-usersInfoTable += '</tr></thead>';
+  const [{balance}] = usersInfoArr;
 
-usersInfoTable += '<tbody>';
+  createTable();
 
-usersInfoArr.forEach(item => {
-  const userInfoArr = Object.values(item);
-  
-  usersInfoTable += '<tr>';
-    userInfoArr.forEach(nestedItem => {
-      usersInfoTable += `<td>${nestedItem}</td>`;
+  function createTable() {
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild(table);
+    createTableHead();
+    createTableBody(usersInfoArr);
+    createTableFoot();
+    tableHolder.appendChild(fragment);
+  }
+
+  function createTableHead() {
+    
+    const tr = document.createElement('tr');
+
+    tableHead.appendChild(tr);
+
+    tableHeadInfoArr.forEach(cell => {
+      const th = document.createElement('th');
+      th.textContent = `${cell}`;
+      tr.appendChild(th);
     });
-  usersInfoTable += '</tr>';
-});
 
-usersInfoTable += '</tbody>';
-usersInfoTable += '<tfoot><tr>';
+    table.appendChild(tableHead);
+  }
 
-usersInfoTable += '</tr></tfoot>';
+  function createTableBody(arr) {
+    arr.forEach(item => {
+      const userInfoArr = Object.values(item);
+      const tr = document.createElement('tr');
+      
+      tableBody.appendChild(tr);
 
-usersInfoTable += '</table>';
+      userInfoArr.forEach(nestedItem => {
+        const td = document.createElement('td');
+        td.textContent = `${nestedItem}`;
+        tr.appendChild(td);
+      });
+    });
+
+    table.appendChild(tableBody);
+  }
+  
+  function createTableFoot() {
+    const tr = document.createElement('tr');
+    const span = document.createElement('span');
+    const strong = document.createElement('strong');
+    const td = document.createElement('td');
+    let totalValue = strong.textContent = calcTotal();
+    
+    tableFoot.appendChild(tr);
+    
+    td.setAttribute('colspan', '4');
+    td.style.textAlign = 'right';
+    span.textContent = 'Total balance: ';
+    td.appendChild(span);
+    strong.textContent = calcTotal();
+    td.appendChild(strong);
+    
+    tr.appendChild(td);
+    table.appendChild(tableFoot);
+  }
+  
+  function calcTotal() {
+    let totalValue = users.reduce((acc, item) => acc += item.balance, 1);
+    
+    return totalValue;
+  }
+  
+  // 7. 
+  // Из презентации “Занятие 7 - Манипуляция DOM. 
+  // Работа с атрибутами.” дополнить функционал для таблицы из задачи 6. 
+  // Создать кнопку которая будет при клике сортировать пользователей по возрастанию или убыванию поля balance 
+  // при этом в кнопке должна показываться стрелка в какую сторону сейчас отсортированы пользователи. 
+  // Иконки можете взять с font awesome, в качестве фреймворка использовался bootstrap.
+
+  let sortBtn = document.createElement('a');
+  let btnText = document.createElement('span');
+  let btnArrow = document.createElement('span');
+  
+  sortBtn.classList.add('btn', 'btn-primary');
+  
+  btnText.textContent = 'Sort ';
+  sortBtn.appendChild(btnText);
+  
+  btnArrow.classList.add('glyphicon', 'glyphicon-arrow-up');
+  sortBtn.appendChild(btnArrow);
+  
+  tableHolder.insertAdjacentElement('afterbegin', sortBtn);
+  
+
+  let sortedusersArr = [];
+  
+  sortBtn.addEventListener('click', sortBalance);
+  
+  function sortBalance() {
+    sortBtn.classList.toggle('min-to-max');
+    
+    if(sortBtn.classList.contains('min-to-max')) {
+      sortedusersArr = usersInfoArr.sort((item1, item2) => (item1.balance - item2.balance));
+    } else {
+      sortedusersArr = usersInfoArr.sort((item1, item2) => (item2.balance - item1.balance));
+    }
+    
+    // console.log(sortedusersArr)
+    // return sortedusersArr;
+    
+    createTableBody(sortedusersArr);
+  }
+
+})(users);
 
 
 
-console.log(balance)
-
-
-tableHolder.innerHTML = usersInfoTable;
-
-// 5.
-// По нажатию на кнопку "btn-msg" должен появиться алерт с тем текстом который находится в атрибуте data-text у кнопки.
-
-let msgBtn = document.querySelector('#btn-msg');
-
-msgBtn.addEventListener('click', showBtnText);
-
-function showBtnText(e) {
-    e.preventDefault();
-    alert(this.innerHTML);
-}
-
-
-// 6. 
-// При нажатии на любой узел документа показать в элементе с id=tag имя тега нажатого элемента.
-
-const allElements = Array.from(document.getElementsByTagName('*'));
-const showTagElement = document.getElementById('tag');
-let tagName = document.createElement('span');
-
-showTagElement.appendChild(tagName);
-
-for (let i = 0; i < allElements.length; i++) {
-    allElements[i].addEventListener('click', showTagName);
-}
-
-function showTagName(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    tagName.innerHTML = ' ' + e.target.tagName;
-}
-
-
-
-
-// 7. 
-// Из презентации “Занятие 7 - Манипуляция DOM. Работа с атрибутами.” дополнить функционал для таблицы из задачи 6. Создать кнопку которая будет при клике сортировать пользователей по возрастанию или убыванию поля balance при этом в кнопке должна показываться стрелка в какую сторону сейчас отсортированы пользователи. Иконки можете взять с font awesome, в качестве фреймворка использовался bootstrap.
-
-// tableHolder.insertBefore(sortBtn,usersInfoTable);
 
 
 });
